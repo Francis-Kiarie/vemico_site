1532cde12ddcd8d7f671338be3c69ed4d102372d
Francis-Kiarie <fkiarie100@gmail.com>
2025-04-23 15:07:43 +0300

    Add animate.css to header.php for enhanced animations

--- header.php
+++ header.php
@@ -22,6 +22,7 @@ if (!isset($pageTitle)) { $pageTitle = 'Home - Vemico Tech'; }
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
     rel="stylesheet"
   >
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-custom navbar-mainbg fixed-top">
